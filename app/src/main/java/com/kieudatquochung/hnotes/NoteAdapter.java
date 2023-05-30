eb868e63461430f6195998dc1b4e306e05f46f87
PC <PC@KIEUHUNG>
2023-05-30 10:41:48 +0700

    Final Commit

--- app/src/main/java/com/kieudatquochung/hnotes/NoteAdapter.java
+++ app/src/main/java/com/kieudatquochung/hnotes/NoteAdapter.java
@@ -17,42 +17,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteViewHolder> {
     Context context;
-    FirestoreRecyclerAdapter<Note, NoteViewHolder> noteAdapter;
-    FirebaseFirestore firebaseFirestore;
-    FirebaseUser firebaseUser;
-
     public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options, Context context) {
         super(options);
         this.context = context;
     }
-
     @Override
     protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull Note note) {
         holder.mTitleTextView.setText(note.title);
         holder.mContentTextView.setText(note.content);
         holder.mTimestampTextView.setText(Utility.timestampToString(note.timestamp));
-        String docId = noteAdapter.getSnapshots().getSnapshot(position).getId();
-
+        String docId = this.getSnapshots().getSnapshot(position).getId();
         ImageView popupButton = holder.itemView.findViewById(R.id.menuPopupButton);
-
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), NoteDetailsActivity.class);
+                Intent intent = new Intent(context, CreateNotesActivity.class);
                 intent.putExtra("title", note.getTitle());
                 intent.putExtra("content", note.getContent());
-                intent.putExtra("noteId", docId);
-
-                v.getContext().startActivity(intent);
-                //Toast.makeText(context.getApplicationContext(), "This is Clicked", Toast.LENGTH_SHORT).show();
+                intent.putExtra("docId", docId);
+                context.startActivity(intent);
             }
         });
         popupButton.setOnClickListener(new View.OnClickListener() {
@@ -63,51 +49,29 @@ public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.Note
                 popupMenu.getMenu().add("Edit").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(@NonNull MenuItem item) {
-                        Intent intent = new Intent(v.getContext(), editNoteActivity.class);
-                        intent.putExtra("title", note.getTitle());
-                        intent.putExtra("content", note.getContent());
-                        intent.putExtra("noteId", docId);
-                        v.getContext().startActivity(intent);
                         return false;
                     }
                 });
                 popupMenu.getMenu().add("Delete").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(@NonNull MenuItem item) {
-                        DocumentReference documentReference = firebaseFirestore.collection("notes")
-                                .document(firebaseUser.getUid()).collection("myNotes").document(docId);
-                        documentReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(v.getContext(), "This is note deleted", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(v.getContext(), "Failed To Delete", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        //Toast.makeText(v.getContext(), "This is note deleted", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context.getApplicationContext(), "This note is deleted", Toast.LENGTH_SHORT).show();
                         return false;
                     }
                 });
                 popupMenu.show();
             }
         });
-
     }
-
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_note_item, parent, false);
         return new NoteViewHolder(view);
     }
-
     class NoteViewHolder extends RecyclerView.ViewHolder
     {
         TextView mTitleTextView, mContentTextView, mTimestampTextView;
-
         public NoteViewHolder(@NonNull View itemView) {
             super(itemView);
             mTitleTextView = itemView.findViewById(R.id.note_Title_Text_View);
